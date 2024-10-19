0b614a69586c518d8c686c175e1cc3a3d26e0eae
sagar-workbase-code <shahsagar03@gmail.com>
2024-10-20 01:05:31 +0530

    ui/ux complete

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,72 @@
+<script>
+	import Category_2 from '$lib/categoryContainer/category-2.svelte';
+	import Footer from '$lib/footer.svelte';
+	import InfoContainer from '$lib/info-container.svelte';
+	import Navbar from '$lib/navbar.svelte';
+	import Container from './container.svelte';
+	const completeEvents = [
+		{
+			id: 1,
+			total: 10000,
+			event: 'Wedding Vendors'
+		},
+		{
+			id: 2,
+			total: 20000,
+			event: 'Annual Weddings'
+		},
+		{
+			id: 3,
+			total: 30800,
+			event: 'Wedding Venues'
+		},
+		{
+			id: 4,
+			total: 1500000,
+			event: 'Monthly Followers'
+		}
+	];
+
+	const formatNumber = (value) => {
+	//tofixed(1) output will always have one decimal place.
+	if (value >= 1000000000) {
+		return `${(value / 1000000000).toFixed(1)}B`;
+	} else if (value >= 1000000) {
+		return `${(value / 1000000).toFixed(1)}M`;
+	} else if (value >= 10000) {
+		return `${(value / 1000).toFixed(1)}K`;
+	} else {
+		return value.toString();
+	}
+};
+</script>
+
+<Navbar />
+<div class="about">
+	<Category_2 title="We Are Occasion" />
+	<Container heading="About Us" order="0" />
+	<div class="py-10">
+		<div
+			class="people text-white mx-auto px-10 text-base bg-[#50808e] grid grid-cols-2 place-items-center md:flex md:justify-center md:items-center gap-y-10 gap-x-14 md:gap-20 py-5"
+		>
+			{#each completeEvents as events}
+				<div>
+					<h1 class="text-xl md:text-2xl lg:text-3xl font-extrabold">
+						{formatNumber(events.total)}
+					</h1>
+					<span class=" font-semibold md:text-lg lg:text-xl">{events.event}</span>
+				</div>
+			{/each}
+		</div>
+	</div>
+	<Container heading="What We offer ?" order="1" />
+	<div class="w-[80%] mx-auto">
+		<InfoContainer />
+	</div>
+	<Container heading="Who We Are ?" order="2" />
+
+	<Footer />
+</div>
+
+<style>
+</style>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,119 @@
+<script>
+	import Category_2 from '$lib/categoryContainer/category-2.svelte';
+	import Footer from '$lib/footer.svelte';
+	import InfoContainer from '$lib/info-container.svelte';
+	import Navbar from '$lib/navbar.svelte';
+
+	function handlesubmit() {
+		alert('Submit');
+	}
+</script>
+
+<Navbar />
+<div class="contact">
+	<Category_2 title="We Are Occasion" />
+	<div class="form-conatiner w-[80%] mx-auto py-10 text-white">
+		<h1 class="font-bold text-3xl pb-10">Say Hello !</h1>
+		<div
+			class="form-details items-center justify-center flex 2xl:gap-56 gap-20 flex-wrap 2xl:flex-nowrap"
+		>
+			<form on:submit={handlesubmit} class="flex flex-col gap-10">
+				<div class="flex justify-center flex-col gap-2">
+					<label class="font-bold text-xl" for="name">Full Name:</label>
+					<input
+						type="text"
+						class="name placeholder:font-bold placeholder:text-lg w-fit py-2 sm:py-3 md:py-4 px-5 sm:pr-32 md:pr-48 rounded-lg placeholder:text-slate-300 bg-opacity-30 backdrop-blur-md bg-white shadow-md"
+						placeholder="Enter Name"
+						required
+					/>
+				</div>
+				<div class="flex justify-center flex-col gap-2">
+					<label class="font-bold text-xl" for="no">Contact Number:</label>
+					<input
+						type="text"
+						class="number placeholder:font-bold placeholder:text-lg w-fit py-2 sm:py-3 md:py-4 px-5 sm:pr-32 md:pr-48 rounded-lg placeholder:text-slate-300 bg-opacity-30 backdrop-blur-md bg-white shadow-md"
+						placeholder="Enter Number"
+						required
+					/>
+				</div>
+
+				<div class="flex justify-center flex-col gap-2">
+					<label class="font-bold text-xl" for="email">Email Address:</label>
+					<input
+						type="email"
+						class="email placeholder:font-bold placeholder:text-lg w-fit py-2 sm:py-3 md:py-4 px-5 sm:pr-32 md:pr-48 rounded-lg placeholder:text-slate-300 bg-opacity-30 backdrop-blur-md bg-white shadow-md"
+						placeholder="Enter Email"
+						required
+					/>
+				</div>
+				<div class="flex justify-center flex-col gap-2">
+					<label class="font-bold text-xl" for="mess">Enter Address:</label>
+					<textarea
+						name="add"
+						placeholder="Enter Your Address ..."
+						rows="1"
+						class="p-5 rounded-xl max-h-72 placeholder:font-bold placeholder:text-lg placeholder:text-slate-300 bg-opacity-30 backdrop-blur-md bg-white shadow-md"
+					></textarea>
+				</div>
+				<div class="flex justify-center flex-col gap-2">
+					<label class="font-bold text-xl" for="mess">Message:</label>
+					<textarea
+						name="message"
+						placeholder="Enter Your Message"
+						class="p-5 rounded-xl max-h-72 min-h-44 placeholder:font-bold placeholder:text-lg placeholder:text-slate-300 bg-opacity-30 backdrop-blur-md bg-white shadow-md"
+					></textarea>
+				</div>
+
+				<button
+					class="bg-[#50808E] hover:bg-[#284e59] hover:shadow-black shadow-inner text-white font-bold text-xl py-3 rounded-xl"
+					>Submit</button
+				>
+			</form>
+
+			<div class="details flex justify-center items-center flex-col gap-10 text-white">
+				<div class="flex flex-col gap-2">
+					<h3 class="text-2xl font-bold">Vendor</h3>
+					<p class="text-xl">
+						If you are a registered vendor or a business looking to promote your brand on our
+						portal, please send in your queries at
+						<a href="/">vendors@comany.com</a>
+					</p>
+				</div>
+				<div class="flex flex-col gap-2">
+					<h3 class="text-2xl font-bold">Marketing Collaborations</h3>
+					<p class="text-xl">
+						For brand collaborations - sponsored content, social media activations etc., please
+						write into
+						<a href="/"> partnerships@comany.com</a>
+					</p>
+				</div>
+				<div class="flex flex-col gap-2">
+					<h3 class="text-2xl font-bold">Wedding Submissions</h3>
+					<p class="text-xl">
+						Occasion features wedding across cultures, styles and budgets. To submit your wedding,
+						kindly email us 15-20 photos at
+						<a href="/">submissions@comany.com</a>
+					</p>
+				</div>
+				<div class="flex flex-col gap-2">
+					<h3 class="text-2xl font-bold">Careers</h3>
+					<p class="text-xl">
+						We are a team of passionate young minds looking to reinvent the wedding space. Please
+						check our careers page for current openings and email us at
+						<a href="/">hr@comany.com</a>
+					</p>
+				</div>
+				<div class="flex flex-col gap-2">
+					<h3 class="text-2xl font-bold">Customers</h3>
+					<p class="text-xl">
+						We love to hear from our precious users. For any feedback or queries simply write in to
+						<a href="/">info@comany.com</a>
+					</p>
+				</div>
+			</div>
+		</div>
+		<InfoContainer />
+	</div>
+
+	<Footer />
+</div>
